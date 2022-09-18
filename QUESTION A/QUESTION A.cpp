a7462c18b554f48731c0073e38390719936eaf34
Dell <Dell@DESKTOP-SII957I>
2022-09-19 02:19:41 +0300

    Add project files.

---
+++ QUESTION A/QUESTION A.cpp
@@ -0,0 +1,43 @@
+// QUESTION A.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <boost/algorithm/string.hpp>
+using namespace boost::algorithm;
+using namespace std;
+
+int main()
+{
+	int i;
+	string a;
+	cout << i << endl;
+	a = "1234567890";
+	i = a.find("9") << endl;
+
+	return 0;
+
+	/*int arr[] = {2,4,85,6,52,1,5,0};
+
+	int max = INT_MIN;
+	int n = 0;
+
+	for (int i = 0; i < n;)
+	{
+		if (arr[i] > max)
+		{
+			if (arr[i] > max)
+				n = i;
+		}
+	}
+	
+	cout << "Maximum value in given array is ";
+	cout << " and its position is " << max.length() << endl;
+
+
+
+	return 0;*/
+
+}
